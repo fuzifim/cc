be76561e0bbd52ca3ae81059c6db675ba3cd9b4b
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-02-01 10:26:36 +0700

    update migration

---
+++ database/migrations/2019_01_31_153833_address.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Address extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('address',500)->nullable();
+            $table->string('address_full',500)->nullable();
+            $table->enum('status', ['active', 'pending','delete']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
